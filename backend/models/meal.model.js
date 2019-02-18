bc7c19a1823e64d3427083dd542df468c1e71b95
komsic <kayodemicheal159@gmail.com>
2019-02-18 11:45:27 +0100

    Add meal service

--- backend/models/meal.model.js
+++ backend/models/meal.model.js
@@ -1,10 +1,10 @@
 class Meal {
-    constructor() {
-        this.id = null;
-        this.name = null;
-        this.quantity = null;
-        this.price = null;
-    }
+  constructor() {
+    this.id = null;
+    this.name = null;
+    this.quantity = null;
+    this.price = null;
+  }
 }
 
 export default Meal;
