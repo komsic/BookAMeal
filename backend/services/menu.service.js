3d983bc69f8f598c3ea265903ec99bb88c30237e
komsic <kayodemicheal159@gmail.com>
2019-02-20 08:29:53 +0100

    Add get-single-menu method to menu service

--- backend/services/menu.service.js
+++ backend/services/menu.service.js
@@ -12,6 +12,9 @@ const MenuService = {
 
     return validMenu;
   },
+  getSingleMenuById(id) {
+    return dummyData.meals.find(menu => menu.id === id) || {};
+  },
 };
 
 export default MenuService;
