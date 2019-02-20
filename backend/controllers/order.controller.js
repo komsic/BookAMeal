69ef1100f81333f8e3bec576a36f96b32318044f
komsic <kayodemicheal159@gmail.com>
2019-02-20 15:43:50 +0100

    Add get all method to order controller

--- backend/controllers/order.controller.js
+++ backend/controllers/order.controller.js
@@ -1,5 +1,14 @@
-const OrderController = {
+import OrderService from '../services/order.service';
 
+const OrderController = {
+  getAllOrder(req, res) {
+    const allOrders = OrderService.getAllMenu();
+
+    return res.json({
+      status: 'success',
+      data: allOrders,
+    }).status(200);
+  },
 };
 
 export default OrderController;
