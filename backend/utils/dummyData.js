2f693ea918e3a921bc0f00f9818797b98f28caf7
komsic <kayodemicheal159@gmail.com>
2019-02-20 04:21:21 +0100

    Add menu data to dummydata

--- backend/utils/dummyData.js
+++ backend/utils/dummyData.js
@@ -7,23 +7,180 @@ export default {
     },
     {
       id: 2,
-      name: 'Akara',
-      price: 1500,
+      name: 'Amala',
+      price: 2500,
     },
     {
       id: 3,
-      name: 'Akara',
-      price: 1500,
+      name: 'Pounded Yam',
+      price: 3500,
     },
     {
       id: 4,
-      name: 'Akara',
-      price: 1500,
+      name: 'Yam With Fried Eggs',
+      price: 2400,
     },
     {
       id: 5,
-      name: 'Akara',
+      name: 'Beans',
       price: 1500,
+    },
+  ],
+  menu: [
+    {
+      id: 1,
+      name: 'Greasy Sae Kitchen',
+      meals: [
+        {
+          id: 1,
+          name: 'Akara',
+          price: 1500,
+        },
+        {
+          id: 2,
+          name: 'Amala',
+          price: 2500,
+        },
+        {
+          id: 3,
+          name: 'Pounded Yam',
+          price: 3500,
+        },
+        {
+          id: 4,
+          name: 'Yam With Fried Eggs',
+          price: 2400,
+        },
+        {
+          id: 5,
+          name: 'Beans',
+          price: 1500,
+        },
+      ],
+    },
+    {
+      id: 2,
+      name: 'Honeydukes',
+      meals: [
+        {
+          id: 1,
+          name: 'Akara',
+          price: 1500,
+        },
+        {
+          id: 2,
+          name: 'Amala',
+          price: 2500,
+        },
+        {
+          id: 3,
+          name: 'Pounded Yam',
+          price: 3500,
+        },
+        {
+          id: 4,
+          name: 'Yam With Fried Eggs',
+          price: 2400,
+        },
+        {
+          id: 5,
+          name: 'Beans',
+          price: 1500,
+        },
+      ],
+    },
+    {
+      id: 3,
+      name: 'Leaky Cauldron',
+      meals: [
+        {
+          id: 1,
+          name: 'Akara',
+          price: 1500,
+        },
+        {
+          id: 2,
+          name: 'Amala',
+          price: 2500,
+        },
+        {
+          id: 3,
+          name: 'Pounded Yam',
+          price: 3500,
+        },
+        {
+          id: 4,
+          name: 'Yam With Fried Eggs',
+          price: 2400,
+        },
+        {
+          id: 5,
+          name: 'Beans',
+          price: 1500,
+        },
+      ],
+    },
+    {
+      id: 4,
+      name: 'The Three Broomsticks',
+      meals: [
+        {
+          id: 1,
+          name: 'Akara',
+          price: 1500,
+        },
+        {
+          id: 2,
+          name: 'Amala',
+          price: 2500,
+        },
+        {
+          id: 3,
+          name: 'Pounded Yam',
+          price: 3500,
+        },
+        {
+          id: 4,
+          name: 'Yam With Fried Eggs',
+          price: 2400,
+        },
+        {
+          id: 5,
+          name: 'Beans',
+          price: 1500,
+        },
+      ],
+    },
+    {
+      id: 5,
+      name: 'The Queen\'s Blessing',
+      meals: [
+        {
+          id: 1,
+          name: 'Akara',
+          price: 1500,
+        },
+        {
+          id: 2,
+          name: 'Amala',
+          price: 2500,
+        },
+        {
+          id: 3,
+          name: 'Pounded Yam',
+          price: 3500,
+        },
+        {
+          id: 4,
+          name: 'Yam With Fried Eggs',
+          price: 2400,
+        },
+        {
+          id: 5,
+          name: 'Beans',
+          price: 1500,
+        },
+      ],
     },
   ],
 };
