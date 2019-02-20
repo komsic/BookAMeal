351c9f66fb7f00e5e1e25f0618011d5fcad27f30
komsic <kayodemicheal159@gmail.com>
2019-02-20 20:10:05 +0100

    Add set up method to menu route

--- backend/routes/menu.route.js
+++ backend/routes/menu.route.js
@@ -5,6 +5,7 @@ import MenuController from '../controllers/menu.controller';
 const router = Router();
 
 router.get('/', MenuController.getAllMenu);
+router.post('/', MenuController.setUpNewMenu);
 router.put('/:id', MenuController.modifySingleMenuById);
 router.get('/:id', MenuController.getSingleMenuById);
 
