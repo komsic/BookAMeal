c45267b0d92586ee2a0104d04727538c883a17fe
komsic <kayodemicheal159@gmail.com>
2019-02-20 07:59:53 +0100

    Add get-all-menu method to menu controller

--- backend/controllers/menu.controller.js
+++ backend/controllers/menu.controller.js
@@ -1,5 +1,14 @@
-const MenuController = {
+import MenuService from '../services/menu.service';
 
+const MenuController = {
+  getAllMenu(req, res) {
+    const allMenu = MenuService.getAllMenu();
+
+    return res.json({
+      status: 'success',
+      data: allMenu,
+    }).status(200);
+  },
 };
 
 export default MenuController;
