9ff95fad97de561b2a58757550ed759c28d19f5b
komsic <kayodemicheal159@gmail.com>
2019-02-20 17:57:28 +0100

    Add get all order endpoint to route

--- backend/routes/order.route.js
+++ backend/routes/order.route.js
@@ -1,5 +1,8 @@
 import { Router } from 'express';
+import OrderController from '../controllers/order.controller';
 
 const router = Router();
+
+router.get('/', OrderController.getAllOrder);
 
 export default router;
