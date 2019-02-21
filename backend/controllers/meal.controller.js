87f3d4c880fbddc1d40a22dcca8f40d5568db120
komsic <kayodemicheal159@gmail.com>
2019-02-21 20:12:46 +0100

    Change meal controller add new meal status code

--- backend/controllers/meal.controller.js
+++ backend/controllers/meal.controller.js
@@ -16,7 +16,7 @@ const MealController = {
     return res.json({
       status: 'success',
       data: createdMeal,
-    }).status(200);
+    }).status(201);
   },
   getSingleMeal(req, res) {
     const { id } = req.params;
