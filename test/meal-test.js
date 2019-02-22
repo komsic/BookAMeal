5680e4d10fda46b0dbdf14bdadd0f616c8a5e460
komsic <kayodemicheal159@gmail.com>
2019-02-22 15:22:09 +0100

    Add get all meals test

--- test/meal-test.js
+++ test/meal-test.js
@@ -1,4 +1,9 @@
 import chai from 'chai';
+import chaiHttp from 'chai-http';
+import app from '../backend/index';
+import dummyData from '../backend/utils/dummyData';
+
+chai.use(chaiHttp);
 
 const { expect } = chai;
 
@@ -6,5 +11,18 @@ const { expect } = chai;
 describe('test', () => {
   it('should return a string', () => {
     expect('ci with travis').to.equal('ci with travis');
+  });
+});
+
+describe('GET /', () => {
+  it('should get all meals', (done) => {
+    const expectedMealLength = dummyData.meals.length;
+    chai.request(app)
+      .get('/api/v1/meals')
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.body).to.have.property('data').with.lengthOf(expectedMealLength);
+        done();
+      });
   });
 });
