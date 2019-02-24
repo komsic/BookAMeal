2d066c87510d609e176548c87442fbeae05a11b8
komsic <kayodemicheal159@gmail.com>
2019-02-24 11:48:30 +0100

    Add get menu by id endpoint test

--- test/menu-test.js
+++ test/menu-test.js
@@ -22,3 +22,23 @@ describe('Menu GET/', () => {
       });
   });
 });
+
+describe('Menu GET/:id', () => {
+  it('it should return a menu with the specified id', (done) => {
+    const specifiedId = 1;
+    const specifiedMenu = dummyData.menu.find(_menu => _menu.id === specifiedId);
+
+    chai.request(app)
+      .get(`${API_PREFIX}/${specifiedId}`)
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.body).to.be.a('object');
+
+        const { data } = res.body;
+        expect(data).to.have.property('name', specifiedMenu.name);
+        expect(data).to.have.property('meals');
+        expect(data).to.have.property('id', specifiedMenu.id);
+        done();
+      });
+  });
+});
