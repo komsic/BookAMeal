f1d9557e3c01081023dd08b5d26c171cefd530c2
komsic <kayodemicheal159@gmail.com>
2019-03-01 12:49:44 +0100

    Add method to get meal schema for meal validation

--- backend/validators/validators.js
+++ backend/validators/validators.js
@@ -8,6 +8,17 @@ class Validator {
   getPositiveNonZeroNumberSchema() {
     return this.Joi.number().integer().greater(0);
   }
+
+  getMealSchema() {
+    return this.Joi.object().options({ abortEarly: false }).keys({
+      id: this.Joi.number().greater(0).allow(null).default(null),
+      name: this.Joi.string().min(1)
+        .when('id', { is: null, then: Joi.required() }),
+      quantity: this.getPositiveNonZeroNumberSchema(),
+      price: this.getPositiveNonZeroNumberSchema(),
+    }).min(2)
+      .label('meal');
+  }
 }
 
 export default Validator;
