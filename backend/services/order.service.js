d59931d944e3cf6369898caf03229d3b1c3616cb
komsic <kayodemicheal159@gmail.com>
2019-03-04 15:24:59 +0100

    Refactor order service to use class

--- backend/services/order.service.js
+++ backend/services/order.service.js
@@ -1,36 +1,53 @@
 import dummyData from '../utils/dummyData';
 import Order from '../models/order.model';
 
-const OrderService = {
-  getAllOrder() {
-    const orders = dummyData.orders.map((order) => {
+class OrderService {
+  static async getAllOrder() {
+    try {
+      const orders = await dummyData.orders.map((order) => {
+        const newOrder = new Order(order);
+        newOrder.id = order.id;
+
+        return newOrder;
+      });
+
+      return orders;
+    } catch (e) {
+      const error = 'An error just occurred while fetching all orders';
+      throw error;
+    }
+  }
+
+  static async modifyOrderById(modifiedOrder, id) {
+    try {
+      const orderToBeModified = await dummyData.orders.find(order => order.id === id);
+      if (orderToBeModified != null) {
+        dummyData.orders[id - 1] = modifiedOrder;
+        dummyData.orders[id - 1].id = id;
+      }
+
+      return dummyData.orders[id - 1];
+    } catch (e) {
+      const error = 'An error just occurred while deleting the meal';
+      throw error;
+    }
+  }
+
+  static async makeNewOrder(order) {
+    try {
+      const orderLength = await dummyData.orders.length;
+      const lastOrderId = await dummyData.orders[orderLength - 1].id;
+
       const newOrder = new Order(order);
-      newOrder.id = order.id;
+      newOrder.id = lastOrderId + 1;
+      await dummyData.orders.push(newOrder);
 
       return newOrder;
-    });
-
-    return orders;
-  },
-  modifyOrderById(modifiedOrder, id) {
-    const orderToBeModified = dummyData.orders.find(order => order.id === id);
-    if (orderToBeModified != null) {
-      dummyData.orders[id - 1] = modifiedOrder;
-      dummyData.orders[id - 1].id = id;
+    } catch (e) {
+      const error = 'An error just occurred while deleting the meal';
+      throw error;
     }
-
-    return dummyData.orders[id - 1];
-  },
-  makeNewOrder(order) {
-    const orderLength = dummyData.orders.length;
-    const lastOrderId = dummyData.orders[orderLength - 1].id;
-
-    const newOrder = new Order(order);
-    newOrder.id = lastOrderId + 1;
-    dummyData.orders.push(newOrder);
-
-    return newOrder;
-  },
-};
+  }
+}
 
 export default OrderService;
