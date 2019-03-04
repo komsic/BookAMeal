eca5e235c58451f38e886641432c8d0012085f83
komsic <kayodemicheal159@gmail.com>
2019-03-04 07:45:19 +0100

    Fix config copy error

--- backend/db/config/config.js
+++ backend/db/config/config.js
@@ -5,7 +5,7 @@ config();
 const Config = Op => ({
   development: {
     username: process.env.DB_USERNAME,
-    password: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
     database: 'database_development',
     host: '127.0.0.1',
     dialect: 'postgres',
