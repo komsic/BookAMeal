e85a17f025ff9c02234b5e881460fb874120ea1b
komsic <kayodemicheal159@gmail.com>
2019-03-04 07:38:47 +0100

    Add meal db model

---
+++ backend/db/migrations/20190304061802-create-meal.js
@@ -0,0 +1,29 @@
+export default {
+  up: (queryInterface, Sequelize) => queryInterface.createTable('Meals', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER,
+    },
+    name: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    quantity: {
+      type: Sequelize.INTEGER,
+    },
+    price: {
+      type: Sequelize.INTEGER,
+    },
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+    },
+  }),
+  down: (queryInterface, Sequelize) => queryInterface.dropTable('Meals'),
+};
