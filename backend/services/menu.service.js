cbb14e51dea0a086f2e9726ff522006c0cf58d7e
komsic <kayodemicheal159@gmail.com>
2019-03-04 15:22:56 +0100

    Refactor menu service to use class

--- backend/services/menu.service.js
+++ backend/services/menu.service.js
@@ -1,38 +1,56 @@
 import dummyData from '../utils/dummyData';
 import Menu from '../models/menu.model';
 
-const MenuService = {
-  getAllMenu() {
-    const validMenu = dummyData.menu.map((menu) => {
+class MenuService {
+  static async getAllMenu() {
+    try {
+      const validMenu = await dummyData.menu.map((menu) => {
+        const newMenu = new Menu(menu.name, menu.meals);
+        newMenu.id = menu.id;
+
+        return newMenu;
+      });
+
+      return validMenu;
+    } catch (e) {
+      const error = 'An error just occurred while fetching the menu';
+      throw error;
+    }
+  }
+
+  static async getSingleMenuById(id) {
+    return await dummyData.menu.find(menu => menu.id === id) || {};
+  }
+
+  static async modifySingleMenuById(modifiedMenu) {
+    try {
+      const menuToBeModify = await dummyData.menu.find(meal => meal.id === modifiedMenu.id);
+      if (menuToBeModify != null) {
+        dummyData.menu[modifiedMenu.id - 1] = modifiedMenu;
+      }
+
+      return dummyData.menu[modifiedMenu.id - 1];
+    } catch (e) {
+      const error = 'An error just occurred while fetching the menu';
+      throw error;
+    }
+  }
+
+  static async setUpNewMenu(menu) {
+    try {
+      const menuLength = dummyData.menu.length;
+      const lastId = dummyData.menu[menuLength - 1].id;
+
       const newMenu = new Menu(menu.name, menu.meals);
-      newMenu.id = menu.id;
+      newMenu.id = lastId + 1;
+      dummyData.menu.push(newMenu);
 
       return newMenu;
-    });
-
-    return validMenu;
-  },
-  getSingleMenuById(id) {
-    return dummyData.menu.find(menu => menu.id === id) || {};
-  },
-  modifySingleMenuById(modifiedMenu) {
-    const menuToBeModify = dummyData.menu.find(meal => meal.id === modifiedMenu.id);
-    if (menuToBeModify != null) {
-      dummyData.menu[modifiedMenu.id - 1] = modifiedMenu;
+    } catch (e) {
+      const error = 'An error just occurred while fetching the menu';
+      throw error;
     }
-
-    return dummyData.menu[modifiedMenu.id - 1];
-  },
-  setUpNewMenu(menu) {
-    const menuLength = dummyData.menu.length;
-    const lastId = dummyData.menu[menuLength - 1].id;
-
-    const newMenu = new Menu(menu.name, menu.meals);
-    newMenu.id = lastId + 1;
-    dummyData.menu.push(newMenu);
-
-    return newMenu;
-  },
-};
+  }
+}
 
 export default MenuService;
