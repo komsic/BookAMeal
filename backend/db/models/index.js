f4100ea207693b4d18d5fc5bf0dea7d798306759
komsic <kayodemicheal159@gmail.com>
2019-03-06 12:04:56 +0100

    Add config and other generated files by sequelize

--- backend/db/models/index.js
+++ backend/db/models/index.js
@@ -5,13 +5,51 @@ import Config from '../config/config';
 
 const env = process.env.NODE_ENV || 'development';
 const { Op } = Sequelize;
-const config = Config(Op)[env];
+const operatorsAliases = {
+  $eq: Op.eq,
+  $ne: Op.ne,
+  $gte: Op.gte,
+  $gt: Op.gt,
+  $lte: Op.lte,
+  $lt: Op.lt,
+  $not: Op.not,
+  $in: Op.in,
+  $notIn: Op.notIn,
+  $is: Op.is,
+  $like: Op.like,
+  $notLike: Op.notLike,
+  $iLike: Op.iLike,
+  $notILike: Op.notILike,
+  $regexp: Op.regexp,
+  $notRegexp: Op.notRegexp,
+  $iRegexp: Op.iRegexp,
+  $notIRegexp: Op.notIRegexp,
+  $between: Op.between,
+  $notBetween: Op.notBetween,
+  $overlap: Op.overlap,
+  $contains: Op.contains,
+  $contained: Op.contained,
+  $adjacent: Op.adjacent,
+  $strictLeft: Op.strictLeft,
+  $strictRight: Op.strictRight,
+  $noExtendRight: Op.noExtendRight,
+  $noExtendLeft: Op.noExtendLeft,
+  $and: Op.and,
+  $or: Op.or,
+  $any: Op.any,
+  $all: Op.all,
+  $values: Op.values,
+  $col: Op.col,
+};
+
+const config = Config[env];
+config.operatorsAliases = operatorsAliases;
 const db = {};
 
 const basename = path.basename(__filename);
 let sequelize;
 if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable]);
+  sequelize = new Sequelize(process.env[config.use_env_variable], { operatorsAliases });
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
