1311d99aa67c85f8cd29864d0d2f8a41b02823a5
komsic <kayodemicheal159@gmail.com>
2019-03-13 19:27:07 +0100

    Test heroku 6

--- backend/db/models/index.js
+++ backend/db/models/index.js
@@ -51,7 +51,7 @@ let sequelize;
 if (config.use_env_variable) {
   console.log(`##############: in prod- ${config.use_env_variable}`);
 
-  sequelize = new Sequelize(process.env[config.use_env_variable], { dialect: 'postgres', operatorsAliases });
+  sequelize = new Sequelize(config.use_env_variable, { dialect: 'postgres', operatorsAliases });
 } else {
   console.log('##############: in other');
   sequelize = new Sequelize(config.database, config.username, config.password, config);
